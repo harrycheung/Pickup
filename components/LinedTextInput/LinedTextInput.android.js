c2b323b5478c920ea543c720f3ff71354f475d0f
Harry Cheung <harry.cheung@gmail.com>
2017-08-21 08:32:49 -0700

    Cleanup android textinput

--- components/LinedTextInput/LinedTextInput.android.js
+++ components/LinedTextInput/LinedTextInput.android.js
@@ -3,21 +3,27 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextInput } from 'react-native';
+import { TextInput, ViewPropTypes } from 'react-native';
+
+import { colors } from '../../config/styles';
 
 const LinedTextInput = props => (
   <TextInput
     {...props}
+    style={[props.style, { height: 44, paddingLeft: 5, alignSelf: 'stretch' }]}
+    selectionColor={'black'}
     underlineColorAndroid={props.borderBottomColor}
   />
 );
 
 LinedTextInput.propTypes = {
+  style: ViewPropTypes.style,
   borderBottomColor: PropTypes.string,
 };
 
 LinedTextInput.defaultProps = {
-  borderBottomColor: 'blue',
+  style: {},
+  borderBottomColor: colors.buttonBackground,
 };
 
 export default LinedTextInput;
