26491fc49127477e42fa041e4ca3b38e5c970b4b
Harry Cheung <harry.cheung@gmail.com>
2017-08-24 12:21:30 -0700

    New building names

--- config/constants.js
+++ config/constants.js
@@ -4,4 +4,4 @@
 export const LoginKey = 'LOGIN_TOKEN';
 export const Levels = ['L1', 'L2', 'L3', 'L4'];
 export const Relationships = ['Parent', 'Family', 'Caregiver', 'Friend'];
-export const Locations = ['Warehouse', 'Playground', 'Front Door'];
+export const Locations = ['Bldg. 1', 'Bldg. 2', 'Bldg. 3', 'Bldg. 4'];
